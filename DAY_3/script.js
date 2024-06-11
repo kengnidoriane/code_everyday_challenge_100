41271455494157d0884eec81643f7aeadefa1372
kengnidoriane <dorianekengni@gmail.com>
2024-06-11 18:18:37 +0100

    Add of the Release version day 3

---
+++ DAY_3/script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container');
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+console.log(close);
+open.addEventListener('click', () => {
+    container.classList.add('show-nav');
+}); 
+close.addEventListener('click', () => {
+    container.classList.remove('show-nav');
+}); 
