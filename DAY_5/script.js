8a87b73fd1b72431989c267e4b65132192d8214c
kengnidoriane <dorianekengni@gmail.com>
2024-06-12 15:50:12 +0100

    Add day5 Custom version

--- DAY_5/script.js
+++ DAY_5/script.js
@@ -1,21 +1,24 @@
 const loadText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
-
+const content = document.querySelector('.content');
+const loading = document.getElementById('loading');
 
 let load = 0;
 
-let interval = setInterval(blurring, 30)
+let interval = setInterval(blurring, 30);
 
-function blurring(){
-    load++
+function blurring() {
+    load++;
 
-    if(load > 99){
-        clearInterval(interval)
+    if (load > 99) {
+        clearInterval(interval);
+        loading.style.display = 'none';
+        content.style.display = 'block';
     }
 
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0)
-    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+    loadText.innerText = `Chargement ${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
 
 const scale = (number, inMin, inMax, outMin, outMax) => 
