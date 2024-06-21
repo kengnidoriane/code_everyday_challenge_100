b8e68254677782dda87b4d1a0485c117a3d944a2
kengnidoriane <dorianekengni@gmail.com>
2024-06-21 09:26:54 +0100

    Add of day 13 tutorial version

--- DAY_13/script.js
+++ DAY_13/script.js
@@ -1,64 +1,75 @@
-// variables declaration
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
 
-const textarea = document.getElementById('inputTextarea');
-const buttonsContainer = document.querySelector('.buttons');
+textarea.focus()
 
+textarea.addEventListener('keyup', (e) => {
+    createTags(e.target.value)
+    
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10);
 
-
-textarea.addEventListener('input', function() {
-
-    // variables pour stocker les valeurs entrees(separes par une virgule) dans un tableau
-    const values = textarea.value.split(',');
-
-    // pour qu'un bouton ne soit creer a chaque changement dans le textarea
-    buttonsContainer.innerHTML = '';
-
-    values.forEach(value => {
-
-        // la fonction trim() est utilise pour retirer les espaces autour de chaque valeur
-
-        const trimmedValue = value.trim();
-
-        if (trimmedValue) {
-            const button = document.createElement('button');
-            button.textContent = trimmedValue;
-            buttonsContainer.appendChild(button);
-        }
-        
-        
-    });
-})
-
-
-
-
-textarea.addEventListener('keydown', function(event) {
-
-    if (event.key === 'Enter') {
-        event.preventDefault(); // Prevent the default action of Enter key
-
-        
-        const buttons = document.querySelectorAll('.buttons button');
-
-        if (buttons.length > 0) {
-            let intervalId = setInterval(() => {
-                const randomIndex = Math.floor(Math.random() * buttons.length);
-                buttons.forEach((button, index) => {
-                    button.style.backgroundColor = index === randomIndex ? '#211df0' : '#f09711';
-                });
-            }, 100);
-
-            setTimeout(() => {
-                clearInterval(intervalId);
-                const selectedButtonIndex = Math.floor(Math.random() * buttons.length);
-                buttons.forEach((button, index) => {
-                    button.style.backgroundColor = index === selectedButtonIndex ? '#211df0' : '#f09711';
-                });
-            }, 3000);
-
-            textarea.value = '';
-        }
+        randomSelect();
     }
 })
+
+function createTags(input) {
+    const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim());
+    tagsEl.innerHTML = '';
+
+    tags.forEach(tag => {
+        const tagEl = document.createElement('span')
+        tagEl.classList.add('tag');
+        tagEl.innerText = tag;
+
+        tagsEl.appendChild(tagEl);
+    });
+    console.log(tags);
+}
+
+
+
+function randomSelect() {
+    const times = 30;
+
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag);
+
+        setTimeout(() => {
+            unHighlightTag(randomTag);
+        }, 100);
+
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag);
+        }, 100);
+    }, times * 100);
+        
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag');
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight');
+}
+
+function unHighlightTag(tag) {
+    tag.classList.remove('highlight');
+}
+
+
+
 
 
